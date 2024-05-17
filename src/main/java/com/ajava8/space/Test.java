e89d80c9419a93e334e54ebdcaf32d7c7da16d5a
Cvenkey <venkatchindukuri58@gmail.com>
2024-05-17 18:38:16 +0530

    Added files

--- src/main/java/com/ajava8/space/Test.java
+++ src/main/java/com/ajava8/space/Test.java
@@ -6,10 +6,22 @@ import java.util.List;
 public class Test {
         public static void main(String[] args) {
             List<Integer> ints = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-
             ints.stream()
                     .filter(new BreakAtCondition<>(i -> i == 6)) // Filter elements until the condition is met
                     .forEach(i -> System.out.println("Java 8 : " + i));
+
+            for(int i=1;i<5;i++){
+                for(int j=1;j<=i ;j++){
+                    System.out.print(j);
+                }
+                System.out.print("\n");
+            }
+            for(int i=3;i>=1;i--){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j);
+                }
+                System.out.print("\n");
+            }
         }
 
         static class BreakAtCondition<T> implements java.util.function.Predicate<T> {
@@ -22,15 +34,11 @@ public class Test {
 
             @Override
             public boolean test(T t) {
-                System.out.println("Test Method");
                 if (!conditionMet ) {
                     conditionMet = predicate.test(t);
                     return true;
                 }
-
                 return false;
             }
         }
-
-
 }
--- src/main/java/com/ajava8/space/strops/StringOperations.java
+++ src/main/java/com/ajava8/space/strops/StringOperations.java
@@ -1,2 +1,22 @@
-package com.ajava8.space.strops;public class StringOperations {
+package com.ajava8.space.strops;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        StringOperations stringOperations = new StringOperations();
+        stringOperations.miscellaneousOps();
+    }
+    private void miscellaneousOps(){
+        String countChars = "Hello World";
+        IntStream intStream = countChars.chars().map(ch -> (char) ch);
+        Stream<Integer> integerStream = intStream.mapToObj(a -> (int) a);
+        List<Integer> collect = integerStream.collect(Collectors.toList());
+        Collections.reverse(collect);
+        collect.forEach(i-> System.out.println((char)i.intValue()));
+    }
 }
